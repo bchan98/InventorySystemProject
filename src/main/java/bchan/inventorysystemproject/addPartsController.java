b6cc9d072d59e5edd7f9f732cd12ff186735f392
Brendan Chan <brendan.mk.chan@gmail.com>
2023-06-27 18:54:31 -0700

    Added in Add Parts window
    Added in JavaFXML files and classes required to move from the main window to the Add Parts window. No functionality in the add parts window has been implemented.

--- src/main/java/bchan/inventorysystemproject/addPartsController.java
+++ src/main/java/bchan/inventorysystemproject/addPartsController.java
@@ -12,18 +12,19 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class AppController {
+public class addPartsController {
     public TextField TextBox;
     @FXML
     private Label welcomeText;
 
-    public void toAddPartsWindow(ActionEvent actionEvent) throws IOException
+    public void toMainWindow(ActionEvent actionEvent) throws IOException
     {
-        Parent root = FXMLLoader.load(getClass().getResource("add-parts.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("main-window.fxml"));
         Stage stage = (Stage) ((Button)actionEvent.getSource()).getScene().getWindow();
         Scene scene = new Scene(root, 600, 400);
-        stage.setTitle ("Add Parts");
+        stage.setTitle ("Inventory Management System");
         stage.setScene(scene);
         stage.show();
     }
+
 }
