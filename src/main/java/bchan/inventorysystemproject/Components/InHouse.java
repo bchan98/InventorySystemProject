1574f9039b3b60b90261d68057a55af6efa3de66
Brendan Chan <brendan.mk.chan@gmail.com>
2023-07-12 02:40:09 -0700

    Added Javadoc and various documentation to InHouse.java.

--- src/main/java/bchan/inventorysystemproject/Components/InHouse.java
+++ src/main/java/bchan/inventorysystemproject/Components/InHouse.java
@@ -3,15 +3,34 @@ package bchan.inventorysystemproject.Components;
 public class InHouse extends Part {
 
     private int machineID;
+
+    /** This constructor inherits from the abstract Parts class, and adds in an additional machine ID parameter
+     *
+     * @param id The part ID
+     * @param name The part name
+     * @param price The part price
+     * @param stock The part stock amount
+     * @param min The part's minimum stock
+     * @param max The part's maximum stock
+     * @param machineID The in-house part's machine ID.
+     */
     public InHouse(int id, String name, double price, int stock, int min, int max, int machineID) {
         super(id, name, price, stock, min, max);
         this.machineID = machineID;
     }
 
+    /** This method returns the machine ID
+     *
+     * @return Returns the machine ID
+     */
     public int getMachineID() {
         return machineID;
     }
 
+    /** This method sets the machine ID
+     *
+     * @param machineID The in-house part's machine ID.
+     */
     public void setMachineID(int machineID) {
         this.machineID = machineID;
     }
