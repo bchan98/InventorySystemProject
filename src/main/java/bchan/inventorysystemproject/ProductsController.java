c70173595bd1fe7f17b5853b808ea0b6a29e9231
Brendan Chan <brendan.mk.chan@gmail.com>
2023-07-12 02:37:46 -0700

    Added Javadoc and various documentation to ProductsController.

--- src/main/java/bchan/inventorysystemproject/ProductsController.java
+++ src/main/java/bchan/inventorysystemproject/ProductsController.java
@@ -63,8 +63,11 @@ public class ProductsController implements Initializable {
     private ObservableList<Part> intAvailablePartList;
 
     @Override
+    /** This method initializes the Products window and populates necessary data. Information is passed from the main window to populate necessary text fields.
+     *
+     */
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        // checks if a product is being added or modified
         if(isTrigger) {
             prodWindowLabel.setText("Add Product");
             prodIDField.setText(String.valueOf(productIDCounter));
@@ -76,16 +79,17 @@ public class ProductsController implements Initializable {
         }
         else {
             prodWindowLabel.setText("Modify Product");
+            // populates data into relevant text fields
             prodIDField.setText(String.valueOf(AppController.sendID));
             prodNameField.setText(AppController.sendName);
             prodInvField.setText(String.valueOf(AppController.sendStock));
             prodPriceField.setText(String.valueOf(AppController.sendPrice));
             prodMinField.setText(String.valueOf(AppController.sendMin));
             prodMaxField.setText(String.valueOf(AppController.sendMax));
-
+            // obtains the associated parts to the relevant product
             Product nuProduct = Inventory.getAllProducts().get((Integer.parseInt(prodIDField.getText())) - 1);
             intAssociatedPartList = nuProduct.getAllAssociatedParts();
-
+            // populates TableView with associated part information
             associatedPartsTable.setItems(intAssociatedPartList);
             assIDCol.setCellValueFactory(new PropertyValueFactory<>("id"));
             assNameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
@@ -93,10 +97,9 @@ public class ProductsController implements Initializable {
             assPriceCol.setCellValueFactory(new PropertyValueFactory<>("price"));
 
         }
+        // populates TableView with available parts to be added
         intAvailablePartList = Inventory.getAllParts();
         FilteredList<Part> showAvailablePartList = new FilteredList<>(intAvailablePartList, Part -> Part != null);
-
-
         availPartsTable.setItems(showAvailablePartList);
         partIDCol.setCellValueFactory(new PropertyValueFactory<>("id"));
         partNameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
@@ -104,6 +107,11 @@ public class ProductsController implements Initializable {
         partPriceCol.setCellValueFactory(new PropertyValueFactory<>("price"));
     }
 
+    /** This method returns the user to the main window.
+     *
+     * @param actionEvent Triggers upon pressing the cancel button.
+     * @throws IOException
+     */
     public void toMainWindow(ActionEvent actionEvent) throws IOException
     {
         Parent root = FXMLLoader.load(getClass().getResource("main-window.fxml"));
@@ -116,6 +124,11 @@ public class ProductsController implements Initializable {
         stage.show();
     }
 
+    /** This method saves changes made to the product. This method carries out a basic logic check to determine all data inputted is valid before saving the product to the allProducts list from the Inventory class.
+     *
+     * @param actionEvent This event triggers upon pressing the saveProductButton.
+     * @throws IOException
+     */
     public void saveChanges(ActionEvent actionEvent) throws IOException {
         // initializes variables
         int setID = Integer.parseInt(prodIDField.getText());
@@ -176,6 +189,7 @@ public class ProductsController implements Initializable {
             allowExecution = true;
         }
 
+        // if logic checks pass, product is saved
         if (allowExecution) {
             setInv = Integer.parseInt(prodInvField.getText());
             setPrice = Double.parseDouble(prodPriceField.getText());
@@ -201,11 +215,20 @@ public class ProductsController implements Initializable {
         }
     }
 
+    /** This method sets the boolean to record whether a product is being added or modified.
+     *
+     * @param trigger This parameter indicates whether a product is being added or modified.
+     */
     public static void setIsAddOrModify(boolean trigger){
         isTrigger = trigger;
     }
 
+    /** This method removes any associated parts from the product. The selected associated part from the associatedParts TableView is removed after a confirmation warning.
+     *
+     * @param actionEvent This event triggers upon pressing the deleteAssPart button.
+     */
     public void removeAssociatePart(ActionEvent actionEvent) {
+        // spawns a warning
         Alert confDel = new Alert(Alert.AlertType.CONFIRMATION);
         confDel.setTitle("Confirm deletion");
         confDel.setHeaderText("Deletion Warning");
@@ -217,9 +240,13 @@ public class ProductsController implements Initializable {
         }
     }
 
+    /** This method adds an associated part to the product. The selected part from the availParts TableView is added to the list of associated parts.
+     *
+     * @param actionEvent This event triggers upon pressing the addAssPart button.
+     */
     public void addAssociatePart(ActionEvent actionEvent) {
         intAssociatedPartList.add((Part) availPartsTable.getSelectionModel().getSelectedItem());
-
+        // checks whether any parts have been added to the product before
         if(isFirstAssItem) {
             FilteredList<Part> showAssociatedPartList = new FilteredList<>(intAssociatedPartList, Part -> Part != null);
 
@@ -229,10 +256,15 @@ public class ProductsController implements Initializable {
             assInvCol.setCellValueFactory(new PropertyValueFactory<>("stock"));
             assPriceCol.setCellValueFactory(new PropertyValueFactory<>("price"));
 
+            // boolean to inform the program that an part has been associated to the product
             isFirstAssItem = false;
         }
     }
 
+    /** This event passes search criteria for any available parts. The data from the availPartField is determined to be an integer or a string, and is then passed to the lookupPart method from the Inventory class and returns a list containing parts that fulfill search criteria.
+     *
+     * @param actionEvent This event triggers upon pressing the Enter key in the availPartField.
+     */
     public void searchAvailPart(ActionEvent actionEvent) {
             ObservableList<Part> nuPartList = FXCollections.observableArrayList();
             nuPartList.clear();
